8890e190e150f0bf7f03e6c0437d135f61b3a63b
Groove-cm8 <74832115+Groove-cm8@users.noreply.github.com>
2020-11-21 21:28:12 +0300

    Update precache-manifest.bc2965b78d73c56a7f1e3ee8560b2857.js

--- precache-manifest.bc2965b78d73c56a7f1e3ee8560b2857.js
+++ precache-manifest.bc2965b78d73c56a7f1e3ee8560b2857.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "a273e55925e2574f0720",
-    "url": "/static/css/2.8cdfe4cf.chunk.css"
+    "url": "/2.8cdfe4cf.chunk.css"
   },
   {
     "revision": "a273e55925e2574f0720",
-    "url": "/static/js/2.30bbb83d.chunk.js"
+    "url": "/2.30bbb83d.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/static/js/2.30bbb83d.chunk.js.LICENSE.txt"
+    "url": "/2.30bbb83d.chunk.js.LICENSE.txt"
   },
   {
     "revision": "885706ae8b2aaad2427d",
-    "url": "/static/js/main.865a5787.chunk.js"
+    "url": "/main.865a5787.chunk.js"
   },
   {
     "revision": "7454b842c22e9e7fa59d",
-    "url": "/static/js/runtime-main.50a7939c.js"
+    "url": "/runtime-main.50a7939c.js"
   }
 ]);
