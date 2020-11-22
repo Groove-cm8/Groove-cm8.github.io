09ec05daa20fabfca595c86a6f8a18abab2b5e3a
Groove-cm8 <74832115+Groove-cm8@users.noreply.github.com>
2020-11-22 18:01:37 +0300

    Add files via upload

---
+++ precache-manifest.0894ac59a60366d0bc4f8375d5f80d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd636a7d7ab3993cfc9fd9a7cfd856d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca76aabbc4d6d3ef697",
+    "url": "/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "2ca76aabbc4d6d3ef697",
+    "url": "/2.09d94ab0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/2.09d94ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2625e914aab7a049ad8",
+    "url": "/main.89088f5f.chunk.js"
+  },
+  {
+    "revision": "7454b842c22e9e7fa59d",
+    "url": "/runtime-main.50a7939c.js"
+  }
+]);
---
+++ precache-manifest.bc2965b78d73c56a7f1e3ee8560b2857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01813ab95b1fb221f9bccf490265bb92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a273e55925e2574f0720",
+    "url": "/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "a273e55925e2574f0720",
+    "url": "/2.30bbb83d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/2.30bbb83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885706ae8b2aaad2427d",
+    "url": "/main.865a5787.chunk.js"
+  },
+  {
+    "revision": "7454b842c22e9e7fa59d",
+    "url": "/runtime-main.50a7939c.js"
+  }
+]);
